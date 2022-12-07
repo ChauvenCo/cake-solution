93fd97230695530900f68164bcc794d70b667185
Damien Boizard <boizarddamien751@gmail.com>
2022-12-07 10:49:00 +0100

    Gâteau

--- cakesolution.py
+++ cakesolution.py
@@ -1,2 +1,2 @@
 def solution(cake):
-    return max([cake.count(cake[:index]) if cake.count(cake[:index]) > 1 and len(cake.replace(cake[:index], '')) == 0 else 1 for index in range(len(cake))])
+    return max([cake.count(cake[:index]) if cake.count(cake[:index]) > 1 and len(cake.replace(cake[:index], '')) == 0 else 1 for index in range(len(cake))], default=1)
--- main.py
+++ main.py
@@ -5,4 +5,4 @@ from cakesolution import *
 for i in p.progressbar(range(5)):
     time.sleep(0.2)
 
-print(solution('abababa'))
+print(solution(''))
