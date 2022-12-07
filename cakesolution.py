33d723e596aed1e1e1eb1726602c221e0ed2c465
Damien Boizard <boizarddamien751@gmail.com>
2022-12-07 14:04:19 +0100

    Classes

--- cakesolution.py
+++ cakesolution.py
@@ -1,2 +1,23 @@
 def solution(cake):
     return max([cake.count(cake[:index]) if cake.count(cake[:index]) > 1 and len(cake.replace(cake[:index], '')) == 0 else 1 for index in range(len(cake))], default=1)
+
+class Person:
+    def __init__(self, name):
+        self.name = name
+    def __repr__(self):
+        return "Person(name=" + self.name + ")"
+
+class PersonXML:
+    @classmethod
+    def save(self, person):
+        print(f'Save the {person} to a XML file')
+
+class PersonJSON:
+    @classmethod
+    def save(self, person):
+        print(f'Save the {person} to a JSON file')
+
+class PersonDatabase:
+    @classmethod
+    def save(self, person):
+        print(f'Save the {person} to a Database')
--- main.py
+++ main.py
@@ -2,7 +2,12 @@ import time
 import progressbar as p
 from cakesolution import *
 
-for i in p.progressbar(range(5)):
-    time.sleep(0.2)
+if __name__ == '__main__':
+    # for i in p.progressbar(range(5)):
+    #     time.sleep(0.2)
+    #
+    # print(solution(''))
 
-print(solution(''))
+    person = Person('John Doe')
+    storage = PersonXML()
+    storage.save(person)
