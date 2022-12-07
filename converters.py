40f0e0d6e68fa81946d31af43919df00f719e9d9
Damien Boizard <boizarddamien751@gmail.com>
2022-12-07 15:21:42 +0100

    Converters

--- converters.py
+++ converters.py
@@ -1,4 +1,11 @@
-class FXConverter:
+from abc import ABC, abstractmethod
+
+class Converter(ABC):
+    @abstractmethod
+    def convert(self, from_currency, to_currency, amount):
+        pass
+
+class FXConverter(Converter):
     def convert(self, from_currency, to_currency, amount):
         print(f'{amount} {from_currency} = {amount * 1.2} {to_currency}')
         return amount * 1.2
